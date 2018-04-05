aebc03440689e78a1318cd7794f42e8fdfad55be
Marc <marc.gluyas@gmail.com>
2018-04-06 01:26:45 +1200

    Add test level and build settings
    Fixed build error in Stage.cs

--- Assets/Scripts/Stage.cs
+++ Assets/Scripts/Stage.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 [ExecuteInEditMode]
 public class Stage : MonoBehaviour
