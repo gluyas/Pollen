1105bb2b9305b0728a2ba70ce6e0aaf493fdb135
Marc <marc.gluyas@gmail.com>
2018-04-17 15:21:07 +1200

    Update Plant to automatically get prefab parent

--- Assets/Scripts/Plant.cs
+++ Assets/Scripts/Plant.cs
@@ -2,9 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class Plant : TileEntity
 {
+	[HideInInspector]
+	public Object Prefab; 
+	
 	public PlantType Type;
+
+	[HideInInspector]
+	public bool PlacedByPlayer = false;
+
+#if UNITY_EDITOR
+	private void OnValidate()
+	{
+		var prefab = PrefabUtility.GetPrefabObject(this.gameObject);
+		if (prefab != null)
+		{
+			Prefab = prefab;
+		}
+		else
+		{
+			Debug.LogAssertionFormat("{0} not an instance of any Prefab", this);
+		}
+	}
+#endif
 }
 
 public enum PlantType
--- Assets/Scripts/Plantable.cs
+++ Assets/Scripts/Plantable.cs
@@ -4,5 +4,5 @@ using UnityEngine;
 
 public class Plantable : TileEntity
 {
-	public Plant Plant;
+	
 }
